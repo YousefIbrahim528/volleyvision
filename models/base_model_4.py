c4afa54bca8b11c6217fdaa4f8701c770a61e0a1
Yousef Ibrahim <iyousef528@gmail.com>
2025-08-12 23:27:41 +0300

    Add LSTM evaluation loop and accuracy calculation for Baseline

--- models/base_model_4.py
+++ models/base_model_4.py
@@ -57,8 +57,16 @@ opt= torch.optim.Adam(lstm.parameters(), lr=0.001)  #using Adam optimiser here, 
                                         
 # Process video data
 matches = [f for f in os.listdir(path)]
-for match in matches:
+l = len(matches)
+limit = int(0.7 * l)
+
+trainingmatches =matches[0:limit]
+
+
+
+for match in trainingmatches:
     clips = [f for f in os.listdir(os.path.join(path, match))]
+    clips = clips[:-1]
     annotation_file = os.path.join(path , match , "annotations.txt")
     video_info, _, _ = annotations.read_file(annotation_file)
 
@@ -86,7 +94,7 @@ for match in matches:
         batch_frames = torch.stack(framestesnors).to(device)
         with torch.no_grad():
             features = model(batch_frames)
-        feature_vectors = features.cpu()
+        feature_vectors = features
 
 
     
@@ -103,6 +111,67 @@ for match in matches:
         lossfunction.backward()
         opt.step()
 
-                        
+#testing 
+lstm.eval()
+
+correct = 0
+total = 0   
+testinggmatches =matches[limit:]
+with torch.no_grad():
         
+    for match in testinggmatches:
+        clips = [f for f in os.listdir(os.path.join(path, match))]
+        clips = clips[:-1]
+        annotation_file = os.path.join(path , match , "annotations.txt")
+        video_info, _, _ = annotations.read_file(annotation_file)
+
+
+        for clip in clips:
+
+            
+        
+
+            framestesnors = []
+            frames = sorted(f for f in os.listdir(os.path.join(path, match, clip)) if f.endswith((".jpg", ".png")))
+            
+            
+            for frame in frames:
+                fullpath = os.path.join(path, match, clip, frame)
+                image = Image.open(fullpath)
+                img_tensor = transform(image)
+                framestesnors.append(img_tensor)
+            
+            
+                    
+
+
+
+            batch_frames = torch.stack(framestesnors).to(device)
+            with torch.no_grad():
+                features = model(batch_frames)
+            feature_vectors = features
+
+
+            feature_vectors = feature_vectors.unsqueeze(0) 
+
+            out = lstm(feature_vectors)
+            id = int(clip)
+            label = video_info[id]["groupactivity"]
+            label = torch.tensor([label], dtype=torch.long, device=device)
+
+
+            predicted = out.argmax(dim=1)
+
+
+            total += label.size(0)
+            correct += (predicted == label).sum().item()
+accuracy = 100 * correct / total
+print(f'Validation Accuracy: {accuracy:.2f}%')
+torch.save(lstm.state_dict(), "baseline4_classifier.pth")
+
+
+#classifier.load_state_dict(torch.load("baseline3_classifier.pth"))
+
+
+
             
